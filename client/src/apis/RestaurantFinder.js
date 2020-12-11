41c12272948d39f6ed5628ad8f9fc5ff983b73d8
dhatGuy <odunsiolakunbi@gmail.com>
2020-12-11 21:49:16 +0100

    change api URL

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://blooming-mesa-76571.herokuapp.com/api/v1/restaurants",
+  baseURL: "https://yelp-cloney.herokuapp.com/api/v1/restaurants",
   timeout: 1000,
 });
